7a1a43e6eb0456ddf351b6dc0e1c7b9429f80962
Vladimirkasperovich <vova.kasper.boy@gmail.com>
2024-11-07 15:10:59 +0300

    feat: The Vowel Code

--- index.js
+++ index.js
@@ -14,4 +14,29 @@ const quickSort = (arr) => {
 	
 	return [...quickSort(less), temp, ...quickSort(greater)];
 }
+
 // console.log(quickSort([10,9,8,7,6,5,4,3,2,1]))
+
+function encode(string) {
+	const vowel = ['a', 'e', 'i', 'o', 'u']
+	return string.replace(/[aeiop]/g, (str) => {
+		const findIndex = vowel.indexOf(str);
+		if (findIndex !== -1){
+			str = findIndex + 1;
+		}
+		return str;
+	})
+}
+
+function decode(string) {
+	const chars = ['a', 'e', 'i', 'o', 'u']
+	return string.replace(/[12345]/g, (str) => {
+		str = chars[str - 1];
+		return str
+	})
+}
+
+// console.log(encode('hello')) //'h2ll4'
+// console.log(encode('How are you today?')) //'H4w 1r2 y45 t4d1y?'
+// console.log(encode('This is an encoding test.')) //'Th3s 3s 1n 2nc4d3ng t2st.'
+// console.log(decode('h2ll4')) //'hello'
