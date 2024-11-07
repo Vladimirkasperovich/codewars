03ba122fd204fecf29bd80a3401f00a0241ec2d4
Vladimirkasperovich <vova.kasper.boy@gmail.com>
2024-11-07 14:20:00 +0300

    feat: add quick sort function

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log(1)
+const quickSort = (arr) => {
+	if (arr.length <= 1) return arr;
+	let temp = arr[0];
+	const less = [];
+	const greater = [];
+	for (let i = 1; i < arr.length; i++) {
+		const current = arr[i];
+		if (current < temp) {
+			less.push(current);
+		} else {
+			greater.push(current);
+		}
+	}
+	
+	return [...quickSort(less), temp, ...quickSort(greater)];
+}
+console.log(quickSort([10,9,8,7,6,5,4,3,2,1]))
