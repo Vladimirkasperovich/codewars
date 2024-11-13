dc82a0211bc7dba77c70b733c01ba42b161a9932
vovak <vova.kasper.boy@gmail.com>
2024-11-13 15:17:19 +0300

    feat: check

--- index.js
+++ index.js
@@ -149,3 +149,7 @@ function goodVsEvil(good, evil) {
 // console.log(goodVsEvil('1 1 1 1 1 1', '1 1 1 1 1 1 1')) //'Battle Result: Evil eradicates all trace of Good'
 // console.log(goodVsEvil('0 0 0 0 0 10', '0 1 1 1 1 0 0')) //'Battle Result: Good triumphs over Evil'
 // console.log(goodVsEvil('1 0 0 0 0 0', '1 0 0 0 0 0 0')) //'Battle Result: No victor on this battle field'
+
+function foo(){
+
+}
