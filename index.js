e2238dab4ee04ad88a7b4d39f7d90f304983478a
vovak <vova.kasper.boy@gmail.com>
2024-11-15 11:57:57 +0300

    feat: Multiply Word in String

--- index.js
+++ index.js
@@ -168,9 +168,39 @@ const toNumberArray = stringarray => stringarray.map(Number);
 
 // console.log(toNumberArray(["1.1","2.2","3.3"]));
 
-const divCon = x => x.reduce((acc, cur) => typeof cur === 'number' ? acc + cur : acc - Number(cur),0)
+const divCon = x => x.reduce((acc, cur) => typeof cur === "number" ? acc + cur : acc - Number(cur), 0);
 
 
 // console.log(divCon([9, 3, "7", "3"])); //2
 // console.log(divCon(["5", "0", 9, 3, 2, 1, "9", 6, 7])); //14
 // console.log(divCon(["3", 6, 6, 0, "5", 8, 5, "6", 2, "0"])); //13
+
+function dup(s) {
+	return s.map(chars => {
+		return chars.split("").filter((char, index, array) => {
+			return char !== array[index - 1];
+		}).join("");
+	});
+}
+
+// console.log(dup(["ccooddddddewwwaaaaarrrrsssss", "piccaninny", "hubbubbubboo"])); //['codewars','picaniny','hubububo']
+// console.log(dup(["abracadabra", "allottee", "assessee"])); //['abracadabra','alote','asese']
+// console.log(dup(["kelless", "keenness"])); //['keles','kenes']
+// console.log(dup(["Woolloomooloo", "flooddoorroommoonlighters", "chuchchi"])); //['Wolomolo','flodoromonlighters','chuchchi']
+// console.log(dup(["adanac", "soonness", "toolless", "ppellee"])); //['adanac','sones','toles','pele']
+// console.log(dup(["callalloo", "feelless", "heelless"])); //['calalo','feles','heles']
+// console.log(dup(["putteellinen", "keenness"])); //['putelinen','kenes']
+// console.log(dup(["kelless", "voorraaddoosspullen", "achcha"])); //['keles','voradospulen','achcha']
+
+
+function modifyMultiply(str, loc, num) {
+	const foundStr = str.split(" ")[loc];
+	let result = "";
+	for (let i = 1; i <= num; i++) {
+		result += foundStr + "-";
+	}
+	return result.slice(0, result.length - 1);
+}
+
+// console.log(modifyMultiply("This is a string", 3, 5)); //"string-string-string-string-string"
+// console.log(modifyMultiply("Creativity is the process of having original ideas that have value. It is a process; it's not random.", 8, 10)); //"that-that-that-that-that-that-that-that-that-that"
