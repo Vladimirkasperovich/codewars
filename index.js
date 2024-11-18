6a337a83c1175cdaf32aa5feef265239f5155bdc
vovak <vova.kasper.boy@gmail.com>
2024-11-18 11:36:34 +0300

    feat: No oddities here

--- index.js
+++ index.js
@@ -281,4 +281,9 @@ function rowWeights(array) {
 // console.log(rowWeights([100, 50])); //[100,50]
 // console.log(rowWeights([50, 60, 70, 80]));//[120,140]
 // console.log(rowWeights([13, 27, 49]));//[62,27]
-console.log(rowWeights([70, 58, 75, 34, 91]));//[236,92]
+// console.log(rowWeights([70, 58, 75, 34, 91]));//[236,92]
+
+function noOdds( values ){
+	// Return all non-odd values
+	return values.filter(el => el % 2 === 0)
+}
