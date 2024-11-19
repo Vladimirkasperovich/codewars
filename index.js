b65b8f91c010c951a08c211b05b92f178eb481ff
vovak <vova.kasper.boy@gmail.com>
2024-11-19 08:50:52 +0300

    feat: Maximum Product

--- index.js
+++ index.js
@@ -328,6 +328,22 @@ function isSortedAndHow(array) {
 	
 }
 
-console.log(isSortedAndHow([1, 2])); //'yes, ascending'
-console.log(isSortedAndHow([15, 7, 3, -8])); //'yes, descending'
-console.log(isSortedAndHow([4, 2, 30])); //'no'
+// console.log(isSortedAndHow([1, 2])); //'yes, ascending'
+// console.log(isSortedAndHow([15, 7, 3, -8])); //'yes, descending'
+// console.log(isSortedAndHow([4, 2, 30])); //'no'
+
+function adjacentElementsProduct(array) {
+	const result = [];
+	for (let i = 0; i < array.length; i++) {
+		if (i < array.length -1){
+			result.push(array[i] *= array[i + 1])
+		}
+	}
+	return Math.max(...result)
+}
+
+// console.log(adjacentElementsProduct([5, 8])); //40
+// console.log(adjacentElementsProduct([1, 2, 3])); //6
+// console.log(adjacentElementsProduct([1, 5, 10, 9])); //90
+// console.log(adjacentElementsProduct([4, 12, 3, 1, 5])); //48
+// console.log(adjacentElementsProduct([5, 1, 2, 3, 1, 4])); //6
