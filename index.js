3125e7c7324216ba771d4bbea72c22654e8aa05f
vovak <vova.kasper.boy@gmail.com>
2024-11-20 11:56:48 +0300

    feat: Coding Meetup #1 - Higher-Order Functions Series - Count the number of JavaScript developers coming from Europe

--- index.js
+++ index.js
@@ -448,7 +448,66 @@ function removeDuplicateWords(s) {
 	for (const element of s.split(" ")) {
 		map[element] = (map[element] || 0) + 1;
 	}
-	return Object.keys(map).join(' ')
+	return Object.keys(map).join(" ");
 }
 
-console.log(removeDuplicateWords("alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta")); //'alpha beta gamma delta'
+// console.log(removeDuplicateWords("alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta")); //'alpha beta gamma delta'
+
+function countDevelopers(list) {
+	// your awesome code here :)
+	return list.filter((l) => l.continent === "Europe" && l.language === 'JavaScript').length
+}
+
+// const list1 = [{firstName: "Noah",
+// 	lastName: "M.",
+// 		country: "Switzerland",
+// 		continent: "Europe",
+// 		age: 19,
+// 		language: "JavaScript"
+// 	},
+// 	{
+// 		firstName: "Maia",
+// 		lastName: "S.",
+// 		country: "Tahiti",
+// 		continent: "Oceania",
+// 		age: 28,
+// 		language: "JavaScript"
+// 	},
+// 	{
+// 		firstName: "Shufen",
+// 		lastName: "L.",
+// 		country: "Taiwan",
+// 		continent: "Asia",
+// 		age: 35,
+// 		language: "HTML"
+// 	},
+// 	{
+// 		firstName: "Sumayah",
+// 		lastName: "M.",
+// 		country: "Tajikistan",
+// 		continent: "Asia",
+// 		age: 30,
+// 		language: "CSS"
+// 	}
+// ];
+// const list2 = [
+// 	{
+// 		firstName: "Oliver",
+// 		lastName: "Q.",
+// 		country: "Australia",
+// 		continent: "Oceania",
+// 		age: 19,
+// 		language: "HTML"
+// 	},
+// 	{
+// 		firstName: "Lukas",
+// 		lastName: "R.",
+// 		country: "Austria",
+// 		continent: "Europe",
+// 		age: 89,
+// 		language: "HTML"
+// 	}
+// ];
+//
+// console.log(countDevelopers(list1)); //1
+// console.log(countDevelopers(list2)); // 2
