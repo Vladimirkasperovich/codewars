154e1711f585eeda6390313d039866f92e812ed0
vovak <vova.kasper.boy@gmail.com>
2024-11-21 11:37:00 +0300

    feat: Encrypt this

--- index.js
+++ index.js
@@ -563,7 +563,7 @@ function getFirstPython(list) {
 
 function removeRotten(bagOfFruits) {
 	// ...
-	if (!bagOfFruits) return []
+	if (!bagOfFruits) return [];
 	return bagOfFruits.map((fruit) => {
 		fruit = fruit.replace(/rotten/g, "");
 		return fruit.toLowerCase();
@@ -572,3 +572,23 @@ function removeRotten(bagOfFruits) {
 
 // console.log(removeRotten(["apple", "rottenBanana", "apple"])); //["apple","banana","apple"]
 // console.log(removeRotten(null));
+
+const encryptThis = (text) => {
+	return text.split(" ").map(word => {
+		const char = word.split("");
+		char[0] = char[0].charCodeAt(0);
+		if (word.length > 2) {
+			const secondChar = char[1];
+			const lastChar = char[char.length - 1];
+			char[1] = lastChar;
+			char[char.length - 1] = secondChar;
+		}
+		return char.join('');
+	}).join(' ');
+};
+
+console.log(encryptThis("A wise old owl lived in an oak")); //"65 119esi 111dl 111lw 108dvei 105n 97n 111ka"
+console.log(encryptThis("The more he saw the less he spoke")); //"84eh 109ero 104e 115wa 116eh 108sse 104e 115eokp"
+console.log(encryptThis("The less he spoke the more he heard")); //"84eh 108sse 104e 115eokp 116eh 109ero 104e 104dare"
+console.log(encryptThis("Why can we not all be like that wise old bird")); //"87yh 99na 119e 110to 97ll 98e 108eki 116tah 119esi 111dl 98dri"
+console.log(encryptThis("Thank you Piotr for all your help")); //"84kanh 121uo 80roti 102ro 97ll 121ruo 104ple"
