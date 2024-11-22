a2427417558686e4fa92129069e900f58ca6a111
vovak <vova.kasper.boy@gmail.com>
2024-11-22 12:59:58 +0300

    feat: Coding Meetup #8 - Higher-Order Functions Series - Will all continents be represented?

--- index.js
+++ index.js
@@ -603,9 +603,72 @@ function bingo(ticket, win) {
 // console.log(bingo([["HGTYRE", 74], ["BE", 66], ["JKTY", 74]], 3)); //'Loser!'
 
 function findSenior(list) {
-	const maxAge = Math.max(...list.map(el => el.age))
-	return list.filter((currentDev) => currentDev.age === maxAge)
+	const maxAge = Math.max(...list.map(el => el.age));
+	return list.filter((currentDev) => currentDev.age === maxAge);
 }
 
 // console.log(findSenior(list1));
 
+function allContinents(list) {
+	// thank you for checking out the Coding Meetup kata :)
+	const mapa = {};
+	list.forEach((el) => {mapa[el.continent] = el.continent; });
+	return ["Africa", "Americas", "Asia", "Europe", "Oceania"].every(el => el === mapa[el])
+}
+
+var list1 = [
+	{
+		firstName: "Fatima",
+		lastName: "A.",
+		country: "Algeria",
+		continent: "Africa",
+		age: 25,
+		language: "JavaScript"
+	},
+	{
+		firstName: "Agustín",
+		lastName: "M.",
+		country: "Chile",
+		continent: "Americas",
+		age: 37,
+		language: "C"
+	},
+	{
+		firstName: "Jing",
+		lastName: "X.",
+		country: "China",
+		continent: "Asia",
+		age: 39,
+		language: "Ruby"
+	},
+	{
+		firstName: "Laia",
+		lastName: "P.",
+		country: "Andorra",
+		continent: "Europe",
+		age: 55,
+		language: "Ruby"
+	},
+	{
+		firstName: "Oliver",
+		lastName: "Q.",
+		country: "Australia",
+		continent: "Oceania",
+		age: 65,
+		language: "PHP"
+	}
+];
+
+var list2 = [
+	{
+		firstName: "Fatima",
+		lastName: "A.",
+		country: "Algeria",
+		continent: "Africa",
+		age: 25,
+		language: "JavaScript"
+	}
+];
+
+console.log(allContinents(list1)); //true
+console.log(allContinents(list2)); //false
