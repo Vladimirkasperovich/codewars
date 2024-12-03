291ef7b7e87751ee37df37db5a8a00282e253305
vovak <vova.kasper.boy@gmail.com>
2024-12-03 11:20:53 +0300

    feat: Largest Elements

--- index.js
+++ index.js
@@ -1035,3 +1035,15 @@ function getMatrix(number) {
 // console.log(getMatrix(1)); //[[1]]
 // console.log(getMatrix(2)); //[[1, 0], [0, 1]]
 // console.log(getMatrix(5)); //[[1, 0, 0, 0, 0], [0, 1, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 0, 1, 0], [0, 0, 0, 0, 1]]
+
+function largest(n, array) {
+	if (!n) return []
+	return array.sort((a,b) => a - b).slice(-n);
+}
+
+// console.log(largest(2, [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]));
+// console.log(largest(0, [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]));
+// console.log(largest(2, [-3, -2, -1, 0, -9, -8, -7, -6, -4, -5]));
+// console.log(largest(3, [5, 1, 5, 2, 3, 1, 2, 3, 5]));
+// console.log(largest(7, [9, 1, 50, 22, 3, 13, 2, 63, 5]));
+// console.log(largest(0, [1, 2, 3, 4, 8, 7, 6, 5]));
