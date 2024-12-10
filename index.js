254458912966fb4d89b9f7f4c1fa5eb208f1d904
vovak <vova.kasper.boy@gmail.com>
2024-12-10 12:00:03 +0300

    feat: Determine if the poker hand is flush

--- index.js
+++ index.js
@@ -1305,3 +1305,14 @@ function solve2(str) {
 // console.log(solve2("your code"));//"edoc ruoy"
 // console.log(solve2("your code rocks"));//"skco redo cruoy"
 // console.log(solve2("i love codewars"));//"s rawe docevoli"
+
+function isFlush(cards) {
+	const arr = cards.map((el) => el.length === 2 ? el[1] : el[2])
+	return [...new Set(arr)].length === 1;
+}
+
+// console.log(isFlush(["AS", "3S", "9S", "KS", "4S"]), true);
+// console.log(isFlush(["AD", "4S", "7H", "KC", "5S"]), false);
+// console.log(isFlush(["AD", "4S", "10H", "KC", "5S"]), false);
+// console.log(isFlush(["QD", "4D", "10D", "KD", "5D"]), true);
+// console.log(isFlush(["10D", "4D", "QD", "KD", "5D"]), true);
