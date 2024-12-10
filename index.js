fcffbdfa2b9281073d76c35bd331107832a7362d
vovak <vova.kasper.boy@gmail.com>
2024-12-10 13:20:50 +0300

    feat:
    Triple trouble

--- index.js
+++ index.js
@@ -1355,9 +1355,9 @@ function tripledouble(num1, num2) {
 	return doubleMatch ? 1 : 0;
 }
 
-console.log(tripledouble(451999277, 41177722899), 1);
-console.log(tripledouble(1222345, 12345), 0);
-console.log(tripledouble(12345, 12345), 0);
-console.log(tripledouble(666789, 12345667), 1);
-console.log(tripledouble(10560002, 100), 1);
-console.log(tripledouble(1112, 122), 0);
+// console.log(tripledouble(451999277, 41177722899), 1);
+// console.log(tripledouble(1222345, 12345), 0);
+// console.log(tripledouble(12345, 12345), 0);
+// console.log(tripledouble(666789, 12345667), 1);
+// console.log(tripledouble(10560002, 100), 1);
+// console.log(tripledouble(1112, 122), 0);
