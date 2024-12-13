c82823b2cf34facd697a95898c7aefb6bab4f92a
vovak <vova.kasper.boy@gmail.com>
2024-12-13 13:24:07 +0300

    feat:Format words into a sentence

--- index.js
+++ index.js
@@ -1596,11 +1596,11 @@ function formatWords(words) {
 	return words.length ? `${words.join(", ")} and ${lastWord}` : lastWord;
 }
 
-console.log(formatWords(["one", "two", "three", "four"]), "one, two, three and four");
-console.log(formatWords(["one"]), "one");
-console.log(formatWords(["one", "", "three"]), "one and three");
-console.log(formatWords(["", "", "three"]), "three");
-console.log(formatWords(["one", "two", ""]), "one and two");
-console.log(formatWords([]), "");
-console.log(formatWords(null), "");
-console.log(formatWords([""]), "");
+// console.log(formatWords(["one", "two", "three", "four"]), "one, two, three and four");
+// console.log(formatWords(["one"]), "one");
+// console.log(formatWords(["one", "", "three"]), "one and three");
+// console.log(formatWords(["", "", "three"]), "three");
+// console.log(formatWords(["one", "two", ""]), "one and two");
+// console.log(formatWords([]), "");
+// console.log(formatWords(null), "");
+// console.log(formatWords([""]), "");
