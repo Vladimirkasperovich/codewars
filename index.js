b731639d12426a7387fc696d0dd7d29f4fcd7494
vovak <vova.kasper.boy@gmail.com>
2024-12-13 12:05:20 +0300

    feat:Sort Out The Men From Boys

--- index.js
+++ index.js
@@ -1534,11 +1534,11 @@ function closedBrackets(s) {
 			minOpen = Math.max(0, minOpen - 1);
 			maxOpen++;
 		}
-		if (maxOpen < 0){
-			return false
+		if (maxOpen < 0) {
+			return false;
 		}
 	}
-	return  minOpen === 0
+	return minOpen === 0;
 }
 
 // console.log(closedBrackets("(J))"), true);
@@ -1551,3 +1551,22 @@ function closedBrackets(s) {
 // console.log(closedBrackets("J)(J"), true);
 // console.log(closedBrackets("(J()J(()(J"), false);
 // console.log(closedBrackets("J(JJ()J((J"), false);
+
+function menFromBoys(arr) {
+	const men = [...new Set(arr)].filter((el) => el % 2 === 0).sort((a, b) => a - b);
+	const boys = [...new Set(arr)].filter((el) => el % 2 !== 0).sort((a, b) => b - a);
+	return men.concat(boys);
+}
+
+// console.log(menFromBoys([7, 3, 14, 17]), [14, 17, 7, 3]);
+// console.log(menFromBoys([2, 43, 95, 90, 37]), [2, 90, 95, 43, 37]);
+// console.log(menFromBoys([20, 33, 50, 34, 43, 46]), [20, 34, 46, 50, 43, 33]);
+// console.log(menFromBoys([82, 91, 72, 76, 76, 100, 85]), [72, 76, 82, 100, 91, 85]);
+// console.log(menFromBoys([2, 15, 17, 15, 2, 10, 10, 17, 1, 1]), [2, 10, 17, 15, 1]);
+// console.log(menFromBoys([-32, -39, -35, -41]), [-32, -35, -39, -41]);
+// console.log(menFromBoys([-64, -71, -63, -66, -65]), [-66, -64, -63, -65, -71]);
+// console.log(menFromBoys([-94, -99, -100, -99, -96, -99]), [-100, -96, -94, -99]);
+// console.log(menFromBoys([-53, -26, -53, -27, -49, -51, -14]), [-26, -14, -27, -49, -51, -53]);
+// console.log(menFromBoys([-17, -45, -15, -33, -85, -56, -86, -30]), [-86, -56, -30, -15, -17, -33, -45, -85]);
+// console.log(menFromBoys([12, 89, -38, -78]), [-78, -38, 12, 89]);
+// console.log(menFromBoys([ 72, 76, 76, 82, 100, 91, 85 ]),[ 72, 76, 76, 82, 100, 91, 85 ]);
