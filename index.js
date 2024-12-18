9e085055f2f4740e94900b04f885f5c1c8adbfc3
vovak <vova.kasper.boy@gmail.com>
2024-12-18 09:30:30 +0300

    feat:The Office III - Broken Photocopier

--- index.js
+++ index.js
@@ -1604,3 +1604,11 @@ function formatWords(words) {
 // console.log(formatWords([]), "");
 // console.log(formatWords(null), "");
 // console.log(formatWords([""]), "");
+
+function broken(x) {
+	return [...x].map((el) => el === "1" ? "0" : "1").join('');
+}
+
+// console.log(broken("1"), "0");
+// console.log(broken("10000000101101111110011001000"), "01111111010010000001100110111");
+// console.log(broken("100010"), "011101");
