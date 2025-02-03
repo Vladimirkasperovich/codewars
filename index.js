61cd5c7927ed335b6e7f94a2e9d8fea2deb5859f
vovak <vova.kasper.boy@gmail.com>
2025-02-03 11:14:33 +0300

    feat: Numbers to Letters

--- index.js
+++ index.js
@@ -2611,4 +2611,14 @@ function cleanString(s) {
 const validPhoneNumber = (phoneNumber) => {
 	return /^\(\d{3}\) \d{3}-\d{4}$/.test(phoneNumber);
 };
-console.log(validPhoneNumber("(123) 456-7890"), true);
+
+// console.log(validPhoneNumber("(123) 456-7890"), true);
+
+function switcher(x) {
+	const alphabet = ' zyxwvutsrqponmlkjihgfedcba!? '
+	return x.map((char) => alphabet[char]).join('')
+}
+
+// console.log(switcher(["24", "12", "23", "22", "4", "26", "9", "8"]), "codewars");
+// console.log(switcher(["25", "7", "8", "4", "14", "23", "8", "25", "23", "29", "16", "16", "4"]), "btswmdsbd kkw");
+// console.log(switcher(["4", "24"]), "wc");
