dad74c666d886bcf6dc704f17699eff52a7b0305
vovak <vova.kasper.boy@gmail.com>
2025-02-19 17:32:50 +0300

    feat:english chars

--- index.js
+++ index.js
@@ -2970,15 +2970,21 @@ function smaller(nums) {
 	const right = new Array(nums.length).fill(0);
 	for (let i = 0; i < nums.length; i++) {
 		for (let j = i + 1; j < nums.length; j++) {
-			if (nums[i] > nums[j]) right[i]++
+			if (nums[i] > nums[j]) right[i]++;
 		}
 	}
-	return right
+	return right;
 }
 
-console.log(smaller([5, 4, 3, 2, 1]), [4, 3, 2, 1, 0]);
+// console.log(smaller([5, 4, 3, 2, 1]), [4, 3, 2, 1, 0]);
 // console.log(smaller([1, 2, 3]), [0, 0, 0]);
 // console.log(smaller([1, 2, 0]), [1, 1, 0]);
 // console.log(smaller([1, 2, 1]), [0, 1, 0]);
 // console.log(smaller([1, 1, -1, 0, 0]), [3, 3, 0, 0, 0]);
 // console.log(smaller([5, 4, 7, 9, 2, 4, 4, 5, 6]), [4, 1, 5, 5, 0, 0, 0, 0, 0]);
+const checkEnglishChars = (words) => {
+	const list = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	return list.split('').every((char) => words.includes(char))
+	
+};
+// console.log(checkEnglishChars("ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
