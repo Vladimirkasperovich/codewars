be7120b9b13e2f3b8b336640e374fe084441e58c
vovak <vova.kasper.boy@gmail.com>
2025-02-24 10:10:04 +0300

    feat: Find how many times did a team from a given country win the Champions League?

--- index.js
+++ index.js
@@ -3073,7 +3073,7 @@ const palindromeChainLength = (n) => {
 		const reversed = +(String(n).split("").reverse().join(""));
 		if (n === reversed) return count;
 		n += reversed;
-		count++
+		count++;
 	}
 };
 
@@ -3082,3 +3082,37 @@ const palindromeChainLength = (n) => {
 // console.log(palindromeChainLength(87), 4);
 // console.log(palindromeChainLength(89), 24);
 // console.log(palindromeChainLength(10), 1);
+
+function countWins(winnerList, country) {
+	return winnerList.reduce((acc, cur) => {
+		if(cur.country === country) acc += 1;
+		return acc
+		
+	}, 0);
+}
+
+// const winnerList1 = [
+// 	{season: "1996–97", team: "Borussia Dortmund", country: "Germany"},
+// 	{season: "1997–98", team: "Real Madrid", country: "Spain"},
+// 	{season: "1998–99", team: "Manchester United", country: "England"},
+// 	{season: "1999–00", team: "Real Madrid", country: "Spain"},
+// 	{season: "2000–01", team: "Bayern Munich", country: "Germany"},
+// 	{season: "2001–02", team: "Real Madrid", country: "Spain"},
+// 	{season: "2002–03", team: "Milan", country: "Italy"},
+// 	{season: "2003–04", team: "Porto", country: "Portugal"},
+// 	{season: "2004–05", team: "Liverpool", country: "England"},
+// 	{season: "2005–06", team: "Barcelona", country: "Spain"},
+// 	{season: "2006–07", team: "Milan", country: "Italy"},
+// 	{season: "2007–08", team: "Manchester United", country: "England"},
+// 	{season: "2008–09", team: "Barcelona", country: "Spain"},
+// 	{season: "2009–10", team: "Internazionale", country: "Italy"},
+// 	{season: "2010–11", team: "Barcelona", country: "Spain"},
+// 	{season: "2011–12", team: "Chelsea", country: "England"},
+// 	{season: "2012–13", team: "Bayern", country: "Germany"},
+// 	{season: "2013–14", team: "Real Madrid", country: "Spain"},
+// 	{season: "2014–15", team: "Barcelona", country: "Spain"},
+// 	{season: "2015–16", team: "Real Madrid", country: "Spain"}
+// ];
+// console.log(countWins(winnerList1, "Portugal")); //1
+// console.log(countWins(winnerList1, "FootLand")); //0
+// console.log(countWins(winnerList1, "Spain")); // 9
