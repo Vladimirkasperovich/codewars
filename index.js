86cf846b76e3c4b176321461126f6a98cf756b9d
vovak <vova.kasper.boy@gmail.com>
2025-02-26 15:10:39 +0300

    feat: binarySearch

--- index.js
+++ index.js
@@ -3303,7 +3303,6 @@ class MinStackCustom {
 	}
 	
 	
-	
 	/**
 	 * @param {number} val
 	 * @return {void}
@@ -3348,3 +3347,24 @@ class MinStackCustom {
 // minStack.pop();
 // minStack.top();    // return 2
 // minStack.getMin(); //return 1
+
+
+function search12(nums, target) {
+	let left = 0;
+	let right = nums.length - 1;
+	
+	while (left <= right) {
+		const mid = Math.floor((left + right) / 2);
+		if (nums[mid] === target) {
+			return mid;
+		} else if (nums[mid] < target) {
+			left = mid + 1;
+		} else {
+			right = mid - 1;
+		}
+	}
+	return - 1
+}
+
+// console.log(search12([0, 1, 2, 3, 5, 7], 3)); //3
+// console.log(search12([0, 1, 2, 3, 5, 7], 8)); //-1
