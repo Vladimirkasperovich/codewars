579dcd5e19835b527c4ec3fd856545ad0b6b2098
vovak <vova.kasper.boy@gmail.com>
2025-03-05 12:29:31 +0300

    feat: Arithmetic progression

--- index.js
+++ index.js
@@ -3512,3 +3512,17 @@ function insertDash(num) {
 // console.log(insertDash(454793), "4547-9-3");
 // console.log(insertDash(123456), "123456");
 // console.log(insertDash(1003567), "1003-567");
+
+function arithmeticSequenceElements(a, d, n) {
+	const result = [];
+	
+	for (let i = 0; i < n; i++) {
+		result.push(a + i * d)
+	}
+	return result.join(", ")
+}
+
+// console.log(arithmeticSequenceElements(1, 2, 5), "1, 3, 5, 7, 9");
+// console.log(arithmeticSequenceElements(1, 0, 5), "1, 1, 1, 1, 1");
+// console.log(arithmeticSequenceElements(1, -3, 10), "1, -2, -5, -8, -11, -14, -17, -20, -23, -26");
+// console.log(arithmeticSequenceElements(100, -10, 10), "100, 90, 80, 70, 60, 50, 40, 30, 20, 10");
