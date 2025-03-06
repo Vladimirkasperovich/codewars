a7578ae4c211cbdc1afea689c4aadeccbbc91b62
vovak <vova.kasper.boy@gmail.com>
2025-03-06 17:46:27 +0300

    feat: Holiday III - Fire on the boat

--- index.js
+++ index.js
@@ -3517,12 +3517,17 @@ function arithmeticSequenceElements(a, d, n) {
 	const result = [];
 	
 	for (let i = 0; i < n; i++) {
-		result.push(a + i * d)
+		result.push(a + i * d);
 	}
-	return result.join(", ")
+	return result.join(", ");
 }
 
 // console.log(arithmeticSequenceElements(1, 2, 5), "1, 3, 5, 7, 9");
 // console.log(arithmeticSequenceElements(1, 0, 5), "1, 1, 1, 1, 1");
 // console.log(arithmeticSequenceElements(1, -3, 10), "1, -2, -5, -8, -11, -14, -17, -20, -23, -26");
 // console.log(arithmeticSequenceElements(100, -10, 10), "100, 90, 80, 70, 60, 50, 40, 30, 20, 10");
+
+
+const fireFight = (str) => str.split(" ").map((item) => item === "Fire" ? "~~" : item).join(" ");
+// console.log(fireFight("Mast Deck Engine Water Fire"), "Mast Deck Engine Water ~~");
+// console.log(fireFight("Fire Deck Engine Sail Deck Fire Fire Fire Rudder Fire Boat Fire Fire Captain"), "~~ Deck Engine Sail Deck ~~ ~~ ~~ Rudder ~~ Boat ~~ ~~ Captain");
