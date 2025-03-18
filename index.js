09c963f4bddfd6b9071d1744a93b9e9a091172c6
vovak <vova.kasper.boy@gmail.com>
2025-03-18 11:00:57 +0300

    feat:Maximum Gap (Array Series #4)

--- index.js
+++ index.js
@@ -3828,3 +3828,24 @@ function listToArray(list) {
 //
 // console.log(listToArray(list1), [1, 2, 3]);
 // console.log(listToArray(list2), ["foo", "bar"]);
+
+
+function maxGap(numbers) {
+	const arr = numbers.sort((a, b) => a - b);
+	const result = [];
+	for (let i = 0; i < arr.length - 1; i++) {
+		result.push(arr[i + 1] - arr[i]);
+	}
+	return Math.max(...result);
+}
+
+// console.log(maxGap([13, 10, 2, 9, 5]), 4);
+// console.log(maxGap([13, 3, 5]), 8);
+// console.log(maxGap([24, 299, 131, 14, 26, 25]), 168);
+// console.log(maxGap([-3, -27, -4, -2]), 23);
+// console.log(maxGap([-7, -42, -809, -14, -12]), 767);
+// console.log(maxGap([12, -5, -7, 0, 290]), 278);
+// console.log(maxGap([-54, 37, 0, 64, -15, 640, 0]), 576);
+// console.log(maxGap([130, 30, 50]), 80);
+// console.log(maxGap([1, 1, 1]), 0);
+// console.log(maxGap([-1, -1, -1]), 0);
