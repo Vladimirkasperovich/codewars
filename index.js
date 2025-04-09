39d140d8b1f4ab3a816644c152393be40d096a42
vovak <vova.kasper.boy@gmail.com>
2025-04-09 14:19:09 +0300

    feat:removeDuplicates

--- index.js
+++ index.js
@@ -4164,7 +4164,20 @@ const removeElement = function (nums, val) {
 			k++;
 		}
 	}
-	return k
+	return k;
 };
 
 // console.log(removeElement([3, 2, 2, 3], 3)); //2
+
+const removeDuplicates = function (nums) {
+	let k = 0;
+	for (let i = 0; i < nums.length; i++) {
+		if (nums[i] !== nums[i - 1]) {
+			nums[k] = nums[i];
+			k++;
+		}
+	}
+	return k;
+};
+
+console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]));
