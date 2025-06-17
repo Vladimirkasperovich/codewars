a8a2a722db87c1f7bdaf9cecedfe6fb8d8ed1045
vovak <vova.kasper.boy@gmail.com>
2025-06-17 14:44:45 +0300

    feat: fibonacci

--- index.js
+++ index.js
@@ -101,3 +101,12 @@ const transformStrToObject = (str) => {
     }
   }
 }*/
+
+
+const fibonacci = (n) => {
+    if (n <= 0) return 0
+    if (n === 1) return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
+}
+// console.log(fibonacci(8)) //21
+// console.log(fibonacci(10)) //55
