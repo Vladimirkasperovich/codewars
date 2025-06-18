0ef52df4ce27718c370791f1eb58c593fae63f99
vovak <vova.kasper.boy@gmail.com>
2025-06-18 13:30:55 +0300

    feat: removeDuplicates

--- index.js
+++ index.js
@@ -152,11 +152,22 @@ const removeElement = (nums, val) => {
             nums[count] = nums[i]
             count++
         }
-
     }
-
 
     return nums
 }
-// console.log(removeElement([3, 2, 2, 3], 3)) //2
+// console.log(removeElement([3, 2, 2, 3], 3)) // 2
 // console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2)) //5
+
+const removeDuplicates = (nums) => {
+    let k = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== nums[i - 1]){
+            nums[k] = nums[i]
+            k++
+        }
+    }
+    return k
+}
+// console.log(removeDuplicates([1, 1, 2])) //2
+// console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4])) //5
