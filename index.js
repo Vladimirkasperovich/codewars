239b80310e223253f36bd700318f01f1585a58a6
vovak <vova.kasper.boy@gmail.com>
2025-06-18 12:39:19 +0300

    feat: removeElement

--- index.js
+++ index.js
@@ -143,3 +143,20 @@ const merge = (nums1, m, nums2, n) => {
 // console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)) //[1,2,2,3,5,6]
 // console.log(merge([1], 1, [], 0)) //[1]
 // console.log(merge([0], 0, [1], 1)) //[1]
+
+
+const removeElement = (nums, val) => {
+    let count = 0
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[count] = nums[i]
+            count++
+        }
+
+    }
+
+
+    return nums
+}
+// console.log(removeElement([3, 2, 2, 3], 3)) //2
+// console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2)) //5
