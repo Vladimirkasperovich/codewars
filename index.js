72168abae78e2d87613d5d8cd0c437404db3dc34
vovak <vova.kasper.boy@gmail.com>
2025-06-20 15:15:59 +0300

    feat: memoize

--- index.js
+++ index.js
@@ -43,3 +43,37 @@ const removeDuplicates = (nums) => {
 // console.log(removeDuplicates([1, 1, 2])) //2, nums = [1,2,_]
 // console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4])) //5, nums = [0,1,2,3,4,_,_,_,_,_]
 // console.log(removeDuplicates([-3, -1, -1, 0, 0, 0, 0, 0, 2]))
+
+const removeDuplicatesMedium = (nums) => {
+    let k = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (k < 2 || nums[i] !== nums[k - 2]) {
+            nums[k] = nums[i]
+            k++
+        }
+    }
+    return k;
+}
+// console.log(removeDuplicatesMedium([1, 1, 1, 2, 2, 3])) //5, nums = [1, 1 ,2,2,3,_]
+// console.log(removeDuplicatesMedium([0, 0, 1, 1, 1, 1, 2, 3, 3])) //7, nums = [0,0,1,1,2,3,3,_,_]
+
+
+const memoize = (fn) => {
+    const cache = new Map();
+    return function (...args) {
+        const key = args ? JSON.stringify(args) : 'no-args'
+        if (cache.has(key)) return cache.get(key);
+        const result = fn(...args);
+        cache.set(key, result);
+        return result;
+    };
+}
+
+// let callCount = 0;
+// const memoizedFn = memoize(function (a, b) {
+//     callCount += 1;
+//     return a + b;
+// })
+// console.log(memoizedFn(2, 3)) // 5
+// console.log(memoizedFn(2, 3)) // 5
+// console.log(callCount) // 1
