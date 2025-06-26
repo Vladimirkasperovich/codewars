c3be1071a201107136acf0040ac754b52144fc15
vovak <vova.kasper.boy@gmail.com>
2025-06-26 11:11:22 +0300

    feat: sortBy

--- index.js
+++ index.js
@@ -372,3 +372,14 @@ Array.prototype.groupBy = function (fn) {
   "true": [6, 7, 8, 9, 10],
   "false": [1, 2, 3, 4, 5]
 }*/
+
+const sortBy = (arr, fn) => {
+    return arr.sort((a, b) => {
+        const A = fn(a);
+        const B = fn(b);
+        return A - B
+    })
+}
+// console.log(sortBy([5, 4, 1, 2, 3], (x) => x)) //[1, 2, 3, 4, 5]
+// console.log(sortBy([{"x": 1}, {"x": 0}, {"x": -1}], (d) => d['x'])) //[{"x": -1}, {"x": 0}, {"x": 1}]
+// console.log(sortBy([[3, 4], [5, 2], [10, 1]], (x) => x[1])) //[[10, 1], [5, 2], [3, 4]]
