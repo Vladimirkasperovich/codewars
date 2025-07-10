2f957cb210167836b2a1d4ce4f603449d9d1c9e4
vovak <vova.kasper.boy@gmail.com>
2025-07-10 14:56:50 +0300

    feat: twoSum

--- index.js
+++ index.js
@@ -310,3 +310,23 @@ function groupBy(arr, key) {
 //   fruit: [{ type: 'fruit', name: 'apple' }, { type: 'fruit', name: 'banana' }],
 //   vegetable: [{ type: 'vegetable', name: 'carrot' }]
 // }
+
+/**
+ * 6. Сумма двух чисел
+ * Функция twoSum(arr, target) возвращает индексы двух чисел, сумма которых равна target.
+ */
+function twoSum(arr, target) {
+    const map = new Map();
+    for (let i = 0; i < arr.length; i++) {
+       const diff = target - arr[i];
+       if (map.has(diff)){
+           return [map.get(diff), i]
+       }
+
+       map.set(arr[i], i)
+    }
+
+    return null
+}
+
+// console.log(twoSum([2, 7, 11, 15], 9)); // [0, 1]
