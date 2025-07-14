2024803a9587a153836337985cd8292a4c928228
vovak <vova.kasper.boy@gmail.com>
2025-07-14 11:47:47 +0300

    feat: flatten

--- index.js
+++ index.js
@@ -8,13 +8,16 @@
  */
 
 const groupBy = (array, callback) => {
-    const seenItems = {};
+    const seenItems = new Map();
 
     for (const item of array) {
-        if (!seenItems[callback(item)]) {
-            seenItems[callback(item)] = [item]
+        const key = callback(item);
+        const group = seenItems.get(key);
+
+        if (!group) {
+            seenItems.set(key, [item])
         } else {
-            seenItems[callback(item)].push(item)
+            group.push(item)
         }
     }
 
@@ -22,5 +25,33 @@ const groupBy = (array, callback) => {
 
 }
 
-console.log(groupBy([6.1, 4.2, 6.3], Math.floor)) // { 6: [6.1, 6.3], 4: [4.2] }
-console.log(groupBy(['one', 'two', 'three'], val => val.length)) // { 3: ['one', 'two'], 5: ['three'] }
+// console.log(groupBy([6.1, 4.2, 6.3], Math.floor)) // { 6: [6.1, 6.3], 4: [4.2] }
+// console.log(groupBy(['one', 'two', 'three'], val => val.length)) // { 3: ['one', 'two'], 5: ['three'] }
+
+
+/**
+ *
+ * Task: Напишите функцию flatten, которая превращает вложенный массив в плоский массив.
+ * Пример: flatten([1, [2, [3, 4], 5]]) должно вернуть [1, 2, 3, 4, 5].
+ *
+ */
+
+const flatten = (array) => {
+    if (!array.length) return array;
+
+    const result = []
+    for (let index = 0; index < array.length; index++) {
+        const current = array[index]
+        if (Array.isArray(current)) {
+            result.push(...flatten(current))
+        } else {
+            result.push(current)
+        }
+    }
+
+    return result
+}
+//
+// console.log(flatten([1, [2, [3, 4], 5]])) // [1, 2, 3, 4, 5]
+// console.log(flatten([[[1]], 2, [[3, [4]]]])) // [1, 2, 3, 4]
+
