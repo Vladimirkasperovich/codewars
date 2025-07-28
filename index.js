edc2814d751356fbf1e48e56ff55e24dfc1d9565
vovak <vova.kasper.boy@gmail.com>
2025-07-28 10:57:50 +0300

    feat: merge

--- index.js
+++ index.js
@@ -457,3 +457,27 @@ const groupByCity = (array) => {
   'Berlin': 'Johan'
 }
 */
+
+const array1 = [[1, 3], [2, 6], [8, 10], [15, 18]]; // [[1, 6], [8, 10], [15, 18]]
+const array2 = [[1, 4], [4, 5]]; // [[1, 5]]
+const array3 = [[11, 12], [2, 3], [5, 7], [1, 4], [8, 10], [6, 8]]; // [[1, 4], [5, 10], [11, 12]]
+
+function merge(intervals) {
+    if (intervals.length < 2) return intervals;
+
+    const sortedInterval = [...intervals].sort((a, b) => a[0] - b[0])
+    const result = [intervals[0]];
+    for (const interval of sortedInterval) {
+        const recent = result[result.length - 1];
+        if (recent[1] >= interval[0]) {
+            recent[1] = Math.max(recent[1], interval[1])
+        } else {
+            result.push(interval)
+        }
+    }
+    return result
+}
+
+// console.log(merge(array1));
+// console.log(merge(array2));
+// console.log(merge(array3));
