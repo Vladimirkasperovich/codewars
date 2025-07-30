31b760c0c2bf889bf70fe0eb5e6b000e67a206a5
vovak <vova.kasper.boy@gmail.com>
2025-07-30 13:29:35 +0300

    feat: fizzBuzz

--- index.js
+++ index.js
@@ -138,4 +138,29 @@ const isAnagram = (s, t) => {
 // console.log(isAnagram("", "")); // true
 
 
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+
+const fizzBuzz = (n) => {
+    const result = [];
+    for (let i = 1; i <= n; i++) {
+       if(i % 3 === 0 && i % 5 === 0){
+           result.push('FizzBuzz')
+       } else if (i % 3 === 0) {
+            result.push('Fizz')
+        } else if (i % 5 === 0) {
+            result.push('Buzz')
+        } else {
+            result.push(`${i}`)
+        }
+
+    }
+    return result;
+};
+
+// console.log(fizzBuzz(5));
+// ["1", "2", "Fizz", "4", "Buzz"]
+
 
